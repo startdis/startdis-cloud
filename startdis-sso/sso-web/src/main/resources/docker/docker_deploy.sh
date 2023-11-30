3fd256d7c655fdb4c364687a2e3d0ed73095ca6c
dianjiu <dianjiuxyz@gmail.com>
2023-12-01 01:43:54 +0800

    feat：更新到私服1.0.0快照版

--- startdis-sso/sso-web/src/main/resources/docker/docker_deploy.sh
+++
@@ -1,7 +0,0 @@
-docker stop sys-web
-docker rm -f sys-web
-docker rmi sys-web
-docker build -t sys-web .
-docker images
-docker run --restart=always -t -dit -p 18081:18081 --name sys-web sys-web
-docker ps
--- startdis-sys/sys-app/src/main/resources/docker/docker_deploy.sh
+++
@@ -1,7 +0,0 @@
-docker stop system-web
-docker rm -f system-web
-docker rmi system-web
-docker build -t system-web .
-docker images
-docker run --restart=always -t -dit -p 18080:18080 -e TZ="Asia/Shanghai" --name system-web system-web
-docker ps
